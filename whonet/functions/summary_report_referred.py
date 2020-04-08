3fb6ee88cadbdb998107d800653d9e1c8789a4fc
Christopher Ocampo <christopher.ocampo27@gmail.com>
2020-04-08 16:51:08 +0800

    fixed bug on data summary report

--- whonet/functions/summary_report_referred.py
+++ whonet/functions/summary_report_referred.py
@@ -1,5 +1,6 @@
 from whonet.models import *
 import pandas as pd
+import numpy as np
 from django.utils.datastructures import MultiValueDictKeyError
 from datetime import datetime
 import os
@@ -9,8 +10,8 @@ from whonet.functions.df_helper import concat_all_df
 dirpath = os.getcwd()
 whonet_data_summary_referred = pd.read_excel(dirpath + '/whonet/static/whonet_xl/whonet_data_summary_referred.xlsx')
 enterobact_all = pd.read_excel(dirpath + '/whonet/static/whonet_xl/whonet_data_summary_referred.xlsx','ENTEROBACTERIACEAE_X_SAL_SHI')
-pae = pd.read_excel(dirpath + '/whonet/static/whonet_xl/whonet_data_summary_referred.xlsx','Pseudomonas aeruginosa')
-aba = pd.read_excel(dirpath + '/whonet/static/whonet_xl/whonet_data_summary_referred.xlsx','Acinetobacter species')
+pae = pd.read_excel(dirpath + '/whonet/static/whonet_xl/whonet_data_summary_referred.xlsx','Pseudomonas_aeruginosa')
+aba = pd.read_excel(dirpath + '/whonet/static/whonet_xl/whonet_data_summary_referred.xlsx','Acinetobacter_species')
 enterobact_all_list = enterobact_all['WHON5_CODE'].values.tolist()
 pae_list = pae['WHON5_CODE'].values.tolist()
 aba_list = aba['WHON5_CODE'].values.tolist()
@@ -60,7 +61,7 @@ def summary_report_referred(file_id,file_name):
     
     for value in enterobact_all_list:
         df_enterobact_all = df_enterobact_all.apply(lambda row: calculate_R_S(row,value,enterobact_all,enterobact_all_list), axis = 1)
-        df_enterobact_all_referred = df_enterobact_all_referred.apply(lambda row: calculate_R_S(row,value,df_enterobact_all_referred,enterobact_all_list), axis = 1)
+        df_enterobact_all_referred = df_enterobact_all_referred.apply(lambda row: calculate_R_S(row,value,enterobact_all,enterobact_all_list), axis = 1)
     
     for value in pae_list:
         df_pae = df_pae.apply(lambda row: calculate_R_S(row,value,pae,pae_list), axis = 1)
@@ -70,38 +71,44 @@ def summary_report_referred(file_id,file_name):
         df_aba = df_aba.apply(lambda row: calculate_R_S(row,value,aba,aba_list), axis = 1)
         df_aba_referred = df_aba_referred.apply(lambda row: calculate_R_S(row,value,aba,aba_list), axis = 1)
     
+    
     if len(df_enterobact_all) > 0:
         df_enterobact_all = df_enterobact_all.apply(lambda row: check_R(row,enterobact_all_list), axis = 1)
-        # df_enterobact_all.dropna(subset = df_enterobact_all.columns, inplace=True)
+        df_enterobact_all = df_enterobact_all[df_enterobact_all['Test'] == 'R']
         df_enterobact_all.dropna(how = 'all',inplace = True)
-        df_enterobact_all = df_enterobact_all.drop(columns=['ORIGIN_REF','FILE_REF','ID','comp','ent_fast'])
+        df_enterobact_all = df_enterobact_all.drop(columns=['ORIGIN_REF','FILE_REF','ID','comp','ent_fast','Test'])
     
     if len(df_enterobact_all_referred) > 0:
         df_enterobact_all_referred = df_enterobact_all_referred.apply(lambda row: check_R(row,enterobact_all_list), axis = 1)
+        df_enterobact_all_referred = df_enterobact_all_referred[df_enterobact_all_referred['Test'] == 'R']
         df_enterobact_all_referred.dropna(how = 'all',inplace = True)
-        df_enterobact_all_referred = df_enterobact_all_referred.drop(columns=['ORIGIN_REF','FILE_REF','ID','comp','ent_fast'])
+        df_enterobact_all_referred = df_enterobact_all_referred.drop(columns=['ORIGIN_REF','FILE_REF','ID','comp','ent_fast','Test'])
     
     if len(df_pae) > 0:
         df_pae = df_pae.apply(lambda row: check_R(row,pae_list), axis = 1)
+        df_pae = df_pae[df_pae['Test'] == 'R']
         df_pae.dropna(how = 'all',inplace = True)
-        df_pae = df_pae.drop(columns=['ORIGIN_REF','FILE_REF','ID','comp','ent_fast'])
+        df_pae = df_pae.drop(columns=['ORIGIN_REF','FILE_REF','ID','comp','ent_fast','Test'])
     
     if len(df_pae_referred) > 0:
         df_pae_referred = df_pae_referred.apply(lambda row: check_R(row,pae_list), axis = 1)
+        df_pae_referred = df_pae_referred[df_pae_referred['Test'] == 'R']
         df_pae_referred.dropna(how = 'all',inplace = True)
-        df_pae_referred = df_pae_referred.drop(columns=['ORIGIN_REF','FILE_REF','ID','comp','ent_fast'])
+        df_pae_referred = df_pae_referred.drop(columns=['ORIGIN_REF','FILE_REF','ID','comp','ent_fast','Test'])
     
     
     if len(df_aba) > 0:
         df_aba = df_aba.apply(lambda row: check_R(row,aba_list), axis = 1)
+        df_aba = df_aba[df_aba['Test'] == 'R']
         df_aba.dropna(how = 'all',inplace = True)
-        df_aba = df_aba.drop(columns=['ORIGIN_REF','FILE_REF','ID','comp','ent_fast'])
+        df_aba = df_aba.drop(columns=['ORIGIN_REF','FILE_REF','ID','comp','ent_fast','Test'])
     
     
     if len(df_aba_referred) > 0:
         df_aba_referred = df_aba_referred.apply(lambda row: check_R(row,aba_list), axis = 1)
+        df_aba_referred = df_aba_referred[df_aba_referred['Test'] == 'R']
         df_aba_referred.dropna(how = 'all',inplace = True)
-        df_aba_referred = df_aba_referred.drop(columns=['ORIGIN_REF','FILE_REF','ID','comp','ent_fast'])
+        df_aba_referred = df_aba_referred.drop(columns=['ORIGIN_REF','FILE_REF','ID','comp','ent_fast','Test'])
     
     
     writer = pd.ExcelWriter('REFERRED_FOR_REVIEW_{}.xlsx'.format(file_name), engine='xlsxwriter')
@@ -125,7 +132,7 @@ def summary_report_referred(file_id,file_name):
     
     writer.save()
     
-    return writer
+    return writer 
 
 
 
@@ -143,7 +150,7 @@ def calculate_R_S(row,value,frame,org_list):
         if row[value].replace('.','').isdigit() == True:
             if frame['R<='][org_list.index(value)] != '':
                 if float(row[value]) <= float(frame['R<='][org_list.index(value)]):
-                    row[value] = 'R'
+                    row[value] = 'R'        
                     return row
                 elif float(row[value]) >= float(frame['S>='][org_list.index(value)]):
                     row[value] = 'S'
@@ -166,6 +173,11 @@ def calculate_R_S(row,value,frame,org_list):
 def check_R(row,value_list):
     for x in value_list:
         if row[x] == 'R':
+            row['Test'] = 'R'
             return row
+        else:
+            row['Test'] = 'S'
+            return row
+    
             
                        
