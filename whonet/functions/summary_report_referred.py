eb509e0d3b1847afcc84c6189ed5fafab0ae4750
Christopher Ocampo <christopher.ocampo27@gmail.com>
2020-04-14 13:30:31 +0800

    checked and completed code with MIC and DISK (for validation of mam cel)

--- whonet/functions/summary_report_referred.py
+++ whonet/functions/summary_report_referred.py
@@ -12,9 +12,20 @@ whonet_data_summary_referred = pd.read_excel(dirpath + '/whonet/static/whonet_xl
 enterobact_all = pd.read_excel(dirpath + '/whonet/static/whonet_xl/whonet_data_summary_referred.xlsx','ENTEROBACTERIACEAE_X_SAL_SHI')
 pae = pd.read_excel(dirpath + '/whonet/static/whonet_xl/whonet_data_summary_referred.xlsx','Pseudomonas_aeruginosa')
 aba = pd.read_excel(dirpath + '/whonet/static/whonet_xl/whonet_data_summary_referred.xlsx','Acinetobacter_species')
+sau = pd.read_excel(dirpath + '/whonet/static/whonet_xl/whonet_data_summary_referred.xlsx','Staphylococcus species')
+ent = pd.read_excel(dirpath + '/whonet/static/whonet_xl/whonet_data_summary_referred.xlsx','Enterococcus species')
+
 enterobact_all_list = enterobact_all['WHON5_CODE'].values.tolist()
 pae_list = pae['WHON5_CODE'].values.tolist()
 aba_list = aba['WHON5_CODE'].values.tolist()
+sau_list = sau['WHON5_CODE'].values.tolist()
+ent_list = ent['WHON5_CODE'].values.tolist()
+
+enterobact_all_list_mic = enterobact_all['WHON5_CODE_MIC'].values.tolist()
+pae_list_mic = pae['WHON5_CODE_MIC'].values.tolist()
+aba_list_mic = aba['WHON5_CODE_MIC'].values.tolist()
+sau_list_mic = sau['WHON5_CODE_MIC'].values.tolist()
+ent_list_mic = ent['WHON5_CODE_MIC'].values.tolist()
 
 def summary_report_referred(file_id,file_name):
     df = concat_all_df(file_id)
@@ -58,57 +69,177 @@ def summary_report_referred(file_id,file_name):
     if len(df_aba_referred) > 0:
         df_aba_referred = df_aba_referred[df_aba_referred['X_REFERRED'] == 1]
         df_aba_referred = df_aba_referred[df_aba_referred['ent_fast'].dt.days >= 0]
+        
+    
+    df_sau = df[df['ORGANISM'].isin(['sau'])]
+    if len(df_sau) > 0:
+        df_sau = df_sau[df_sau['X_REFERRED'] != 1]
+        df_sau = df_sau[df_sau['ent_fast'].dt.days >= 0]
+    
+    
+    df_sau_referred = df[df['ORGANISM'].isin(['sau'])]
+    if len(df_sau_referred) > 0:
+        df_sau_referred = df_sau_referred[df_sau_referred['X_REFERRED'] == 1]
+        df_sau_referred = df_sau_referred[df_sau_referred['ent_fast'].dt.days >= 0]
+        
+          
+    df_ent = df[df['ORGANISM'].isin(['efa','efm'])]
+    if len(df_ent) > 0:
+        df_ent = df_ent[df_ent['X_REFERRED'] != 1]
+        df_ent = df_ent[df_ent['ent_fast'].dt.days >= 0]
+    
+    df_ent_referred = df[df['ORGANISM'].isin(['efa','efm'])]
+    if len(df_ent_referred) > 0:
+        df_ent_referred = df_ent_referred[df_ent_referred['X_REFERRED'] != 1]
+        df_ent_referred = df_ent_referred[df_ent_referred['ent_fast'].dt.days >= 0]
+    
+    
+    
     
     for value in enterobact_all_list:
         df_enterobact_all = df_enterobact_all.apply(lambda row: calculate_R_S(row,value,enterobact_all,enterobact_all_list), axis = 1)
         df_enterobact_all_referred = df_enterobact_all_referred.apply(lambda row: calculate_R_S(row,value,enterobact_all,enterobact_all_list), axis = 1)
     
+    for value in enterobact_all_list_mic:
+        df_enterobact_all = df_enterobact_all.apply(lambda row: calculate_R_S_MIC(row,value,enterobact_all,enterobact_all_list_mic), axis = 1)
+        df_enterobact_all_referred = df_enterobact_all_referred.apply(lambda row: calculate_R_S_MIC(row,value,enterobact_all,enterobact_all_list_mic), axis = 1)
+        
     for value in pae_list:
         df_pae = df_pae.apply(lambda row: calculate_R_S(row,value,pae,pae_list), axis = 1)
         df_pae_referred = df_pae_referred.apply(lambda row: calculate_R_S(row,value,pae,pae_list), axis = 1)
     
+    for value in pae_list_mic:
+        df_pae = df_pae.apply(lambda row: calculate_R_S(row,value,pae,pae_list_mic), axis = 1)
+        df_pae_referred = df_pae_referred.apply(lambda row: calculate_R_S(row,value,pae,pae_list_mic), axis = 1)
+    
     for value in aba_list:
         df_aba = df_aba.apply(lambda row: calculate_R_S(row,value,aba,aba_list), axis = 1)
         df_aba_referred = df_aba_referred.apply(lambda row: calculate_R_S(row,value,aba,aba_list), axis = 1)
+        
+    for value in aba_list_mic:
+        df_aba = df_aba.apply(lambda row: calculate_R_S(row,value,aba,aba_list_mic), axis = 1)
+        df_aba_referred = df_aba_referred.apply(lambda row: calculate_R_S(row,value,aba,aba_list_mic), axis = 1)
+    
+    for value in sau_list:
+        df_sau = df_sau.apply(lambda row: calculate_R_S(row,value,sau,sau_list), axis = 1)
+        df_sau_referred = df_sau_referred.apply(lambda row: calculate_R_S(row,value,sau,sau_list), axis = 1)
+    
+    for value in sau_list_mic:
+        df_sau = df_sau.apply(lambda row: calculate_R_S(row,value,sau,sau_list_mic), axis = 1)
+        df_sau_referred = df_sau_referred.apply(lambda row: calculate_R_S(row,value,sau,sau_list_mic), axis = 1)
+    
+    for value in ent_list:
+        df_ent = df_ent.apply(lambda row: calculate_R_S(row,value,ent,ent_list), axis = 1)
+        df_ent_referred = df_ent_referred.apply(lambda row: calculate_R_S(row,value,ent,ent_list), axis = 1)
+    
+    for value in ent_list_mic:
+        df_ent = df_ent.apply(lambda row: calculate_R_S(row,value,ent,ent_list_mic), axis = 1)
+        df_ent_referred = df_ent_referred.apply(lambda row: calculate_R_S(row,value,ent,ent_list_mic), axis = 1)
     
     
     if len(df_enterobact_all) > 0:
-        df_enterobact_all = df_enterobact_all.apply(lambda row: check_R(row,enterobact_all_list), axis = 1)
+        df_enterobact_all_concat = df_enterobact_all.apply(lambda row: check_R(row,enterobact_all_list), axis = 1)
+        df_enterobact_all = df_enterobact_all.apply(lambda row: check_R(row,enterobact_all_list_mic), axis = 1)
+        frames = [df_enterobact_all_concat,df_enterobact_all]
+        df_enterobact_all= pd.concat(frames)
         df_enterobact_all = df_enterobact_all[df_enterobact_all['Test'] == 'R']
         df_enterobact_all.dropna(how = 'all',inplace = True)
+        df_enterobact_all = df_enterobact_all.drop_duplicates(['PATIENT_ID','LAST_NAME','SPEC_NUM','SPEC_DATE','SPEC_TYPE','ORGANISM'], keep=False)
         df_enterobact_all = df_enterobact_all.drop(columns=['ORIGIN_REF','FILE_REF','ID','comp','ent_fast','Test'])
     
     if len(df_enterobact_all_referred) > 0:
+        df_enterobact_all_referred_concat = df_enterobact_all_referred.apply(lambda row: check_R(row,enterobact_all_list_mic), axis = 1)
         df_enterobact_all_referred = df_enterobact_all_referred.apply(lambda row: check_R(row,enterobact_all_list), axis = 1)
+        frames = [df_enterobact_all_referred,df_enterobact_all_referred_concat]
         df_enterobact_all_referred = df_enterobact_all_referred[df_enterobact_all_referred['Test'] == 'R']
         df_enterobact_all_referred.dropna(how = 'all',inplace = True)
+        df_enterobact_all_referred = df_enterobact_all_referred.drop_duplicates(['PATIENT_ID','LAST_NAME','SPEC_NUM','SPEC_DATE','SPEC_TYPE','ORGANISM'], keep=False)
         df_enterobact_all_referred = df_enterobact_all_referred.drop(columns=['ORIGIN_REF','FILE_REF','ID','comp','ent_fast','Test'])
     
     if len(df_pae) > 0:
+        df_pae_concat = df_pae.apply(lambda row: check_R(row,pae_list_mic), axis = 1)
         df_pae = df_pae.apply(lambda row: check_R(row,pae_list), axis = 1)
+        frames = [df_pae,df_pae_concat]
+        df_pae = pd.concat(frames)
         df_pae = df_pae[df_pae['Test'] == 'R']
         df_pae.dropna(how = 'all',inplace = True)
+        df_pae = df_pae.drop_duplicates(['PATIENT_ID','LAST_NAME','SPEC_NUM','SPEC_DATE','SPEC_TYPE','ORGANISM'], keep=False)
         df_pae = df_pae.drop(columns=['ORIGIN_REF','FILE_REF','ID','comp','ent_fast','Test'])
     
     if len(df_pae_referred) > 0:
+        df_pae_referred_concat = df_pae_referred.apply(lambda row: check_R(row,pae_list_mic), axis = 1)
         df_pae_referred = df_pae_referred.apply(lambda row: check_R(row,pae_list), axis = 1)
+        frames = [df_pae_referred,df_pae_referred_concat]
+        df_pae_referred = pd.concat(frames)
         df_pae_referred = df_pae_referred[df_pae_referred['Test'] == 'R']
         df_pae_referred.dropna(how = 'all',inplace = True)
+        df_pae_referred = df_pae_referred.drop_duplicates(['PATIENT_ID','LAST_NAME','SPEC_NUM','SPEC_DATE','SPEC_TYPE','ORGANISM'], keep=False)
         df_pae_referred = df_pae_referred.drop(columns=['ORIGIN_REF','FILE_REF','ID','comp','ent_fast','Test'])
     
     
     if len(df_aba) > 0:
+        df_aba_concat = df_aba.apply(lambda row: check_R(row,aba_list_mic), axis = 1)
         df_aba = df_aba.apply(lambda row: check_R(row,aba_list), axis = 1)
+        frames = [df_aba,df_aba_concat]
+        df_aba = pd.concat(frames)
         df_aba = df_aba[df_aba['Test'] == 'R']
         df_aba.dropna(how = 'all',inplace = True)
+        df_aba = df_aba.drop_duplicates(['PATIENT_ID','LAST_NAME','SPEC_NUM','SPEC_DATE','SPEC_TYPE','ORGANISM'], keep=False)
         df_aba = df_aba.drop(columns=['ORIGIN_REF','FILE_REF','ID','comp','ent_fast','Test'])
     
     
     if len(df_aba_referred) > 0:
+        df_aba_referred_concat = df_aba_referred.apply(lambda row: check_R(row,aba_list_mic), axis = 1)
         df_aba_referred = df_aba_referred.apply(lambda row: check_R(row,aba_list), axis = 1)
+        frames = [df_aba_referred,df_aba_referred_concat]
+        df_aba_referred = pd.concat(frames)
         df_aba_referred = df_aba_referred[df_aba_referred['Test'] == 'R']
         df_aba_referred.dropna(how = 'all',inplace = True)
+        df_aba_referred = df_aba_referred.drop_duplicates(['PATIENT_ID','LAST_NAME','SPEC_NUM','SPEC_DATE','SPEC_TYPE','ORGANISM'], keep=False)
         df_aba_referred = df_aba_referred.drop(columns=['ORIGIN_REF','FILE_REF','ID','comp','ent_fast','Test'])
+        
+    
+    if len(df_sau) > 0:
+        df_sau_concat = df_sau.apply(lambda row: check_R(row,sau_list_mic), axis = 1)
+        df_sau = df_sau.apply(lambda row: check_R(row,sau_list), axis = 1)
+        frames = [df_sau,df_sau_concat]
+        df_sau = pd.concat(frames)
+        df_sau = df_sau[df_sau['Test'] == 'R']
+        df_sau.dropna(how = 'all',inplace = True)
+        df_sau = df_sau.drop_duplicates(['PATIENT_ID','LAST_NAME','SPEC_NUM','SPEC_DATE','SPEC_TYPE','ORGANISM'], keep=False)
+        df_sau = df_sau.drop(columns=['ORIGIN_REF','FILE_REF','ID','comp','ent_fast','Test'])
+    
+    if len(df_sau_referred) > 0:
+        df_sau_referred_concat = df_sau_referred.apply(lambda row: check_R(row,sau_list_mic), axis = 1)
+        df_sau_referred = df_sau_referred.apply(lambda row: check_R(row,sau_list), axis = 1)
+        frames = [df_sau_referred,df_sau_referred_concat]
+        df_sau_referred = pd.concat(frames)
+        df_sau_referred = df_sau_referred[df_sau_referred['Test'] == 'R']
+        df_sau_referred.dropna(how = 'all',inplace = True)
+        df_sau_referred = df_sau_referred.drop_duplicates(['PATIENT_ID','LAST_NAME','SPEC_NUM','SPEC_DATE','SPEC_TYPE','ORGANISM'], keep=False)
+        df_sau_referred = df_sau_referred.drop(columns=['ORIGIN_REF','FILE_REF','ID','comp','ent_fast','Test'])
+        
+        
+        
+    if len(df_ent) > 0:
+        df_ent_concat = df_ent.apply(lambda row: check_R(row,ent_list_mic), axis = 1)
+        df_ent = df_ent.apply(lambda row: check_R(row,ent_list), axis = 1)
+        frames = [df_ent,df_ent_concat]
+        df_ent = pd.concat(frames)
+        df_ent = df_ent[df_ent['Test'] == 'R']
+        df_ent.dropna(how = 'all',inplace = True)
+        df_ent = df_ent.drop_duplicates(['PATIENT_ID','LAST_NAME','SPEC_NUM','SPEC_DATE','SPEC_TYPE','ORGANISM'], keep=False)
+        df_ent = df_ent.drop(columns=['ORIGIN_REF','FILE_REF','ID','comp','ent_fast','Test'])
+    
+    if len(df_ent_referred) > 0:
+        df_ent_referred_concat = df_ent_referred.apply(lambda row: check_R(row,ent_list_mic), axis = 1)
+        df_ent_referred = df_ent_referred.apply(lambda row: check_R(row,ent_list), axis = 1)
+        frames = [df_ent_referred,df_ent_referred_concat]
+        df_ent_referred = pd.concat(frames)
+        df_ent_referred = df_ent_referred[df_ent_referred['Test'] == 'R']
+        df_ent_referred.dropna(how = 'all',inplace = True)
+        df_ent_referred = df_ent_referred.drop_duplicates(['PATIENT_ID','LAST_NAME','SPEC_NUM','SPEC_DATE','SPEC_TYPE','ORGANISM'], keep=False)
+        df_ent_referred = df_ent_referred.drop(columns=['ORIGIN_REF','FILE_REF','ID','comp','ent_fast','Test'])
     
     
     writer = pd.ExcelWriter('REFERRED_FOR_REVIEW_{}.xlsx'.format(file_name), engine='xlsxwriter')
@@ -129,6 +260,18 @@ def summary_report_referred(file_id,file_name):
     
     if len(df_aba_referred) > 0:
         df_aba_referred.to_excel(writer, sheet_name='aba_referred', index=False)
+    
+    if len(df_sau) > 0:
+        df_sau.to_excel(writer, sheet_name='sau', index=False)
+    
+    if len(df_sau_referred) > 0:
+        df_sau_referred.to_excel(writer, sheet_name='sau_referred', index=False)
+    
+    if len(df_ent) > 0:
+        df_ent.to_excel(writer,sheet_name='ent',index=False)
+        
+    if len(df_ent_referred) > 0:
+        df_ent_referred.to_excel(writer,sheet_name='ent_referred',index=False)
     
     writer.save()
     
@@ -169,6 +312,24 @@ def calculate_R_S(row,value,frame,org_list):
         else:
             return row
 
+
+def calculate_R_S_MIC(row,value,frame,org_list):
+    row[value] = row[value].replace('>=','')
+    row[value] = row[value].replace('<=','')
+    row[value] = row[value].replace('>','')
+    row[value] = row[value].replace('<','')
+    if row[value].replace('.','').isdigit() == True:
+            if frame['R>='][org_list.index(value)] != '':
+                if float(row[value]) >= float(frame['R>='][org_list.index(value)]):
+                    row[value] = 'R'        
+                    return row
+                elif float(row[value]) <= float(frame['S<='][org_list.index(value)]):
+                    row[value] = 'S'
+                    return row
+                else:
+                    return row
+    else:
+        return row
                 
 def check_R(row,value_list):
     for x in value_list:
